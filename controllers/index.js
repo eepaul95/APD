533d79f5f0d394586f0a8938dc77732c088da0d3
Miguel Angel Rodriguez <Marodriguez148@gmail.com>
2018-03-22 08:01:54 -0400

    added an about page. Not complete

--- controllers/index.js
+++ controllers/index.js
@@ -4,6 +4,6 @@ const router = express.Router();
 
 router.use('/', require('./home'));
 router.use('/states', require('./states'));
-
+router.use('/about', require('./about'));
 
 module.exports = router;
