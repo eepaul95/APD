4a56ac1da93109130d6b39d6aa9f5f4e27c8944c
shateeshb <bhugwansing.shateesh@gmail.com>
2018-04-12 09:30:24 -0400

    set up project for mocha/chai testing

---
+++ test/test.js
@@ -0,0 +1,16 @@
+var expect  = require('chai').expect;
+var request = require('request');
+
+/*
+TO DO: Ask the rest of the team how each controller functions work, so I can write test functions based on what to expect. 
+*/
+/*
+describe('home.js test', function(){
+  it('About page content', function(done) {
+    request('http://localhost:8080/about' , function(error, response, body) {
+        expect(response.statusCode).to.equal(404);
+        done();
+    });
+  });
+});
+*/
