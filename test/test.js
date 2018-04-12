4fb71a87d1e22cf3aab76d76673d2010e4076cc3
shateeshb <bhugwansing.shateesh@gmail.com>
2018-04-12 16:40:26 -0400

    also added sinon dependency for testing

--- test/test.js
+++ test/test.js
@@ -1,16 +1,23 @@
-var expect  = require('chai').expect;
+var assert = require('chai').assert;
+var should = request('chai').should();
+var expect = request('chai').expect;
 var request = require('request');
 
 /*
-TO DO: Ask the rest of the team how each controller functions work, so I can write test functions based on what to expect. 
+TO DO: Ask the rest of the team how each controller functions work, so I can write test functions based on what to expect.
 */
-/*
+
 describe('home.js test', function(){
-  it('About page content', function(done) {
-    request('http://localhost:8080/about' , function(error, response, body) {
-        expect(response.statusCode).to.equal(404);
+
+  it('Main page content', function(done) {
+
+    // this should be some code that will execute the function(s) in home.js
+    /*
+    request('http://localhost:8080' , function(error, response, body) {
+        expect(body).to.equal('Hello World');
         done();
     });
+  */
+
   });
 });
-*/
