28d61e7ee4c73545e5f4a43329cf6bdd90525984
shateeshb <bhugwansing.shateesh@gmail.com>
2018-04-15 13:25:07 -0400

    "Closes #18"

--- controllers/home.js
+++ controllers/home.js
@@ -1,14 +1,12 @@
 const express = require('express');
-
 const router = express.Router();
 
 var myHome = require('./myHome');
-
 router.get('/', myHome);
 
 
-/*
 //Elie's code
+/*
 router.get('/', (req, res) => {
   res.render('home');
 });
