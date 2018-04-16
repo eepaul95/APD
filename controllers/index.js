7ce15a1fc6f75ce73d9ea54fb53ad40190824b62
Elie Elisee Paul <marcoht023@hotmail.com>
2018-04-16 19:52:28 -0400

    Create the route for politicians, will be able to render given the necessary view.

--- controllers/index.js
+++ controllers/index.js
@@ -4,5 +4,6 @@ const router = express.Router();
 router.use('/', require('./home'));
 router.use('/states', require('./states'));
 router.use('/trending', require('./trending'));
+router.use('/politicians', require('./politicians'));
 
 module.exports = router;
