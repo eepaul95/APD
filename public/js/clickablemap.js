6f74808b8f45c93e0eb9be15675b18bc16c6e4f5
Simon Chuu <simonorj@outlook.com>
2018-05-01 03:22:05 -0400

    Fix hover/click state colors
    yay.

--- public/js/clickablemap.js
+++ public/js/clickablemap.js
@@ -1,9 +1,16 @@
 (function(){
 "use strict;"
-    const domMap = $('#clickableMap'),
-          domStateName = $('#stateName'),
-          domContainer = $('#mapContainer');
-    var previousWidth = domContainer.width();
+    var fill = {
+        normal: "#b8b8b8",
+        highlight: "#00a6f7",
+        active: "#007bff"
+    }
+    
+    var domMap = $('#clickableMap'),
+        domStateName = $('#stateName'),
+        domContainer = $('#mapContainer');
+    var previousWidth = domContainer.width(),
+        mapStates;
     
     // Refer to https://github.com/markmarkoh/datamaps#default-options
     var map = new Datamap({
@@ -11,21 +18,23 @@
         element: domMap[0],
         responsive: true,
         fills: {
-            defaultFill: '#b8b8b8'
+            defaultFill: fill.normal
         },
         geographyConfig: {
             borderColor: '#ffffff',
             popupOnHover: false,
-            highlightFillColor: '#00a6f7',
+            highlightFillColor: fill.highlight,
             highlightBorderColor: '#ffffff',
             highlightBorderWidth: 1
         },
         done: function(d) {
-            var all = d.svg.selectAll('.datamaps-subunit');
-            console.log(all);
-            all.on('mouseover', mapMouseover); // When you hover on the map
-            all.on('click', mapClick); // When you click on the map
-            
+            mapStates = d.svg.selectAll('.datamaps-subunit')
+                    .on('mouseover',    mapMouseover) // When you hover on the map
+                    .on('mouseleave',   mapMouseleave) // When mouse leaves
+                    .on('mousedown',    mapMousedown) // Press and hold
+                    .on('mouseup',      mapMouseup) // Release the mouse
+                    .on('click',        mapClick) // When you click on the map
+                    ;
         }
     });
     
@@ -34,20 +43,34 @@
     }); // display labels
     
     
-    // Map state hover
-    function mapMouseover(g) {
-        // why does the highlight get disabled ;-;
-        // TODO: Attempt to remake the highlight thing
-        // TODO: Delete text when mouse leaves
-        
+    // Map highlight transition function in main.css
+    var fill, clicked;
+    
+    function mapMouseover(g,n) {
+        $(mapStates[0][n]).css('fill',fill.highlight);
         domStateName.html("<b>" + g.properties.name + "</b>");
     }
     
-    // Map state clicks
-    function mapClick(g) {
-        // TODO: Click confirmation, loading screen or something
-        
+    function mapMouseleave(g,n) {
+        $(mapStates[0][n]).css('fill',fill.normal)
+        domStateName.html("");
+    }
+    
+    function mapMousedown(g,n) {
+        if (clicked != n)
+            $(mapStates[0][clicked]).css('fill',fill.normal)
+        $(mapStates[0][clicked = n]).css('fill',fill.active)
+    }
+    
+    function mapMouseup(g,n) {
+        if (clicked == n)
+            mapStates.on('mouseover',function(){}).on('mouseleave',function(){});
+    }
+    
+    function mapClick(g,n) {
+        // TODO: loading screen or something?
         window.location.href = "/states/" + g.properties.name;
+        domStateName.html("<b>" + g.properties.name + "</b>");
     }
     
     // Enable resizing map when window resizes
