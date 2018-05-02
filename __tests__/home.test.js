256b3e996e3f840df93073f23d1ffa4e4e2cc77a
shateeshb <bhugwansing.shateesh@gmail.com>
2018-05-01 20:48:45 -0400

    fixing up home.test.js

--- __tests__/home.test.js
+++ __tests__/home.test.js
@@ -1,12 +1,21 @@
 const home = require('../controllers/home');
 const myHome = require('../controllers/myHome');
+const request = require('request');
+
 
 describe('Home test', () => {
-  test('basic home test', ()=> {
+  test('Test: home render', ()=> {
     var res, req, spy;
     req = res = {};
     spy = res.render = jest.fn();
     myHome(req, res);
     expect(spy).toBeCalled();
   });
+
+  test('Test: Error != 404', () =>{
+    request('http://localhost:8000' , function(error, response, body) {
+    expect(error).not.toBe(404);
+    });
+  });
+
 });
