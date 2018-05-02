003db53cc758ac976b1f04a8ba70b7d98bf3e347
shateeshb <bhugwansing.shateesh@gmail.com>
2018-05-02 12:08:37 -0400

    still trying to figure out how to test async calls in this lambda function format

---
+++ __tests__/about.test.js
@@ -0,0 +1,21 @@
+const about = require('../controllers/about');
+const request = require('request');
+
+/*
+describe('About test', () => {
+  test('Test: about render', ()=> {
+    var res, req, spy;
+    req = res = {};
+    spy = res.render = jest.fn();
+    about.router.get(req,res);
+    expect(spy).toBeCalled();
+  });
+
+  test('Test: Error != 404', () =>{
+    request('http://localhost:8000/about' , function(error, response, body) {
+    expect(error).not.toBe(404);
+    });
+  });
+
+});
+*/
