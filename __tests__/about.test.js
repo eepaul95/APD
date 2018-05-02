0b8cb7fb75eb767a109e9c737af1cec83bb94e12
shateeshb <bhugwansing.shateesh@gmail.com>
2018-05-02 17:34:15 -0400

    still figuring things out

--- __tests__/about.test.js
+++ __tests__/about.test.js
@@ -1,17 +1,23 @@
 const about = require('../controllers/about');
 const request = require('request');
+const supertest = require('supertest');
+const httpMocks = require('node-mocks-http');
 
 
 describe('About test', () => {
-  /*
-  test('Test: about render', ()=> {
-    var res, req, spy;
-    req = res = {};
-    spy = res.render = jest.fn();
-    about.router.get(req,res);
-    expect(spy).toBeCalled();
-  });
-  */
+  test('It should response the GET method', async () => {
+    // const req = httpMocks.createRequest();
+    // const res = httpMocks.createResponse();
+
+    // const response = await supertest(about).get('/');
+    // expect(response.statusCode).toBe(200);
+
+    return about.router.get(req, res).then((response) => {
+  // response.message
+  assert.equal(typeof response.message, 'string', 'message is a string')});
+});
+
+
   test('Test: Error != 404', () =>{
     request('http://localhost:8000/about' , function(error, response, body) {
     expect(error).not.toBe(404);
