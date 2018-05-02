cc860e204845f20532b54ddd3bb544c8b7989d38
Elie Elisee Paul <marcoht023@hotmail.com>
2018-05-02 11:29:18 -0400

    add wrapper for API and its test

--- middlewares/apiHelpers.js
+++ middlewares/apiHelpers.js
@@ -6,36 +6,34 @@ const client = new Congress( apiKey );
 
 const apiHelpers = {};
 
-apiHelpers.getAllSenateByState(stateAbbr){
-	let senators = [];
-	client.membersCurrentByStateOrDistrict({
-     chamber: 'senate',
-     state: stateAbbr
-    }).then(senatorsResult => {
-    	senators = senatorsResult.results;
-    });
+apiHelpers.getAllSenateByState= (stateAbbr) => {
+	let senators = client.membersCurrentByStateOrDistrict({
+				     chamber: 'senate',
+				     state: stateAbbr
+				    }).then(senatorsInfo => {
+				    	return senatorsInfo.results;
+				    })
+	
     return senators;
 }
 
-apiHelpers.getRepresentativeByDistrict(stateAbbr, districtNum){
-	let representative = [];
-	client.membersCurrentByStateOrDistrict({
+apiHelpers.getRepresentativeByDistrict = (stateAbbr, districtNum) => {
+	let representative = client.membersCurrentByStateOrDistrict({
 		chamber: 'house',
 		state: stateAbbr,
 		district: districtNum
 	}).then(representativeResult => {
-		representative = representativeResult.results[0];
+		return representativeResult.results;
 	});
 	return representative;
 }
 
 
-apiHelpers.getCongressMemberById(id){
-	let congressMember = [];
-	client.memberBioAndRoles({
+apiHelpers.getCongressMemberById = (id) => {
+	let congressMember = client.memberBioAndRoles({
 		memberId: id
 	}).then(congressMemberResult => {
-		congressMember = congressMemberResult.results[0];
+		return congressMemberResult.results;
 	});
 	return congressMember;
 }
