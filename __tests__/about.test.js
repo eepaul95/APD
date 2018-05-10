3e2b87c1c7ce4a5a06bf4ccc6f90de303272ae55
shateeshb <bhugwansing.shateesh@gmail.com>
2018-05-09 20:24:22 -0400

    cant figure out how to test anonymous functions.

--- __tests__/about.test.js
+++ __tests__/about.test.js
@@ -5,6 +5,9 @@ const httpMocks = require('node-mocks-http');
 
 
 describe('About test', () => {
+/*
+
+============== TO DO: try to figure out how to test anonymous functions. Please ask Shateesh to explain
   test('It should response the GET method', async () => {
     // const req = httpMocks.createRequest();
     // const res = httpMocks.createResponse();
@@ -12,11 +15,16 @@ describe('About test', () => {
     // const response = await supertest(about).get('/');
     // expect(response.statusCode).toBe(200);
 
-    return about.router.get(req, res).then((response) => {
-  // response.message
-  assert.equal(typeof response.message, 'string', 'message is a string')});
-});
 
+    var res, req, spy;
+    req = res = {};
+    spy = res.render = jest.fn();
+    return about.get('/', spy).then((response) => {
+  // response.message
+  expect(spy).toBeCalled()});
+
+});
+*/
 
   test('Test: Error != 404', () =>{
     request('http://localhost:8000/about' , function(error, response, body) {
