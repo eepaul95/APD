bfc80ade373de7844688e5519eb2136a55980450
Miguel Angel Rodriguez <Marodriguez148@gmail.com>
2018-05-12 11:56:11 -0400

    Added news api, tried to display all articles but for some reason can not get them to appear in the html

--- controllers/myHome.js
+++ controllers/myHome.js
@@ -1,5 +1,23 @@
+const NewsAPI = require('newsapi');
+const newsapi = new NewsAPI('69f83c84a33c475584137f95b7eef274');
+
 function myHome(req, res){
-  res.render('home');
+  newsapi.v2.topHeadlines({
+  category: 'politics',
+  language: 'en',
+  country: 'us'
+}).then(response => {
+  let articles = [];
+  let articlesLength = response.articles.length;
+  for (i = 0; i < articlesLength; i++){
+    articles.push(response.articles[i]);
+  }
+  console.log(articles[1].title);
+  res.render('home', {newsArticles: articles});
+}).catch((err) => {
+    console.log(err);
+    res.render('/');
+});
 }
 
 module.exports = myHome;
